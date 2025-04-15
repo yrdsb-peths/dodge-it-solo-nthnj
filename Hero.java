ef5b10ccfca663b2735967b14a4ca88118755d44
nthnj <349045377@gapps.yrdsb.ca>
2025-04-15 14:20:18 -0400

    mouse clicked and new hero

---
+++ Hero.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Hero extends Actor
+{
+    public void act()
+    {
+        if(Greenfoot.mouseClicked()) {
+            
+        }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,10 @@
-import greenfoot.*;
+    import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
     }
 }
